d4dc50dc8d299e4375f9737906a01e0536e47bf6
Eliel Martins <elieuprata@gmail.com>
2024-01-13 17:51:53 -0300

    add small-card ande menu-bar and adjust css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BigCardComponent, MenuTitleComponent],
+  imports: [
+    BigCardComponent,
+    SmallCardComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

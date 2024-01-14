fad6b7bfe361a380016da896024d1871966aa478
Eliel Martins <elieuprata@gmail.com>
2024-01-14 16:59:50 -0300

    create content page

---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css',
+})
+export class ContentComponent {
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+}

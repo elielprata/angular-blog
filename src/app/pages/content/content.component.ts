1d98a6d3a09c96fd470996edff043aabd5ce9ea0
Eliel Martins <elieuprata@gmail.com>
2024-01-15 23:08:36 -0300

    receiving data dynamically

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -12,4 +13,22 @@ export class ContentComponent {
   photoCover: string = '';
   contentTitle: string = '';
   contentDescription: string = '';
+
+  private id: string | null = '0';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id === id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+  }
 }

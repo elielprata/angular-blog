4dcc63d7fe9eaeb57eea5b28710468248bd9cdb7
Eliel Martins <elieuprata@gmail.com>
2024-01-15 23:18:56 -0300

    image size adjusted

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -11,8 +11,7 @@ export const dataFake = [
     id: '2',
     photoCover:
       'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhIZHSko5OXqh-gzFHvpf4inNGDNDLD5U0moEaiH_s7MmD5cP2RKBL5pd3TWVrw8CBaXZcv1B53j7RLPfU-ux_3zKC3fBDGg2fVc_czLQs-m-ONZcoa46Rmegadllkg9iqZu8XPhj-81zi2gzyssDffUADlVThwo2M-A8hz7Py4vEIAExQ_S1jLwqVCp2mK/s1024/ia.png',
-    title:
-      'A Revolução da Inteligência Artificial: Impactos Profundos nos Dias Atuais',
+    title: 'A Revolução da Inteligência Artificial',
     description:
       'A inteligência artificial (IA) tornou-se uma força transformadora nos dias atuais, moldando uma ampla gama de setores e nossa vida cotidiana. Desde a assistência virtual em nossos smartphones até o diagnóstico médico avançado e a revolução na indústria, a IA se estabeleceu como um elemento crucial em nosso mundo moderno. Nesta matéria, exploraremos os impactos profundos que a inteligência artificial causa na sociedade e na economia contemporânea.',
   },
@@ -20,8 +19,7 @@ export const dataFake = [
     id: '3',
     photoCover:
       'https://mid-redecidadedigital.ici.curitiba.org.br/2023/7/jpg/capa/00007584.jpg',
-    title:
-      'NIC.br investe em Inteligência Artificial para diagnosticar a Internet no Brasil',
+    title: 'NIC.br investe em Inteligência Artificial',
     description:
       'O Núcleo de Informação e Coordenação do Ponto BR (NIC.br) desenvolveu um modelo estatístico para inteligência artificial (IA) que se alimenta de dados do Sistema de Medição de Tráfego Internet (SIMET) para determinar o tipo de acesso à rede, seja em instituições de ensino, estabelecimentos de saúde ou mesmo em domicílios no país.',
   },
